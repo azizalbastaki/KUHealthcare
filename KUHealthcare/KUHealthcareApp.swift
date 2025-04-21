bfb1049b81a809cb5e30811d58480933f2d84c45
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2025-04-21 11:28:08 +0400

    Initial Commit

---
+++ KUHealthcare/KUHealthcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUHealthcareApp.swift
+//  KUHealthcare
+//
+//  Created by Abdulaziz Albastaki on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct KUHealthcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KUHealthcareTests/KUHealthcareTests.swift
@@ -0,0 +1,17 @@
+//
+//  KUHealthcareTests.swift
+//  KUHealthcareTests
+//
+//  Created by Abdulaziz Albastaki on 21/04/2025.
+//
+
+import Testing
+@testable import KUHealthcare
+
+struct KUHealthcareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
